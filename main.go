7a5dd7d9ca8754a2d7c563b3058db9b551ed7172
napnie <c.nitith@gmail.com>
2018-11-19 13:59:42 +0700

    fix main

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/guitarpawat/portscan/api"
 	"github.com/guitarpawat/portscan/scanner"
 )
 
 func main() {
-	ipv4, _, _ := scanner.NSLookUp("www.google.com")
-	fmt.Println(scanner.GetOpenPorts(ipv4, 80, 443))
-	ipv6, _, _ := scanner.NSLookUp("www.google.com")
-	fmt.Println(scanner.GetOpenPorts(ipv6, 80, 443))
+	ipv4, ipv6, _ := scanner.NSLookUp("www.google.com")
+	knowPorts := api.GetKnownPorts()
+	fmt.Println(knowPorts)
+	fmt.Println(scanner.GetOpenPorts(ipv4, knowPorts...))
+	fmt.Println(scanner.GetOpenPorts(ipv6, knowPorts...))
 }
