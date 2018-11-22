fd0a0245ceff02f33287b84156bd6ae759342034
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-11-22 20:09:01 +0700

    Fixed docs lint

--- web/routes.go
+++ web/routes.go
@@ -1,4 +1,4 @@
-// Serve the RESTful API service server for port scanner
+// Package web serves the RESTful API service server for port scanner
 package web
 
 import (
@@ -9,7 +9,7 @@ import (
 	"net/http"
 )
 
-// Serve HTTP server with specified host:port
+// ListenAndServe serves HTTP server with specified host:port
 func ListenAndServe(serve string) error {
 	r := mux.NewRouter()
 	r.HandleFunc("/api/scan", handlePostScanRequest).Methods("POST")
