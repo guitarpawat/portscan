3f6a33beb59ee79f1c62c48e59810bc839dfd510
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-11-27 00:12:35 +0700

    auto open browser

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"github.com/guitarpawat/portscan/api/cache"
 	"github.com/guitarpawat/portscan/web"
+	"github.com/pkg/browser"
 	"log"
+	"time"
 )
 
 func main() {
 	defer cache.CloseDB()
 
 	log.Println("START:\t", "running on port 80")
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		err := browser.OpenURL("http://localhost:80")
+		if err != nil {
+			log.Println(err)
+		}
+	}()
 
 	log.Fatalln(web.ListenAndServe(":80"))
 }
